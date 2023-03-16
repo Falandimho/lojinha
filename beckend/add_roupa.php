dc4b3e2695eec42aa87f8a24ebd0cce9db28452a
Falandimho <anthonygames311@gmail.com>
2023-03-16 13:39:28 -0300

    opa

--- beckend/add_roupa.php
+++ beckend/add_roupa.php
@@ -28,23 +28,6 @@ $status = $_POST['sit'];
 $arquivo = $_FILES['foto'];
 
 
-if ($arquivo['error']) {
-    die("Falha ao enviar arquivo");
-}
-
-
-$pasta = "arquivos/";
-$nomeArq = $arquivo['name'];
-$novoNomeArq = uniqid();
-$extensao = strtolower(pathinfo($nomeArq, PATHINFO_EXTENSION));
-
-if ($extensao != "jpg" && $extensao = !"png" && $extensao = !"jpeg") {
-    die("Tipo de arquivo não suportado");
-}
-
-$path = "../" . $pasta . $novoNomeArq . "." . $extensao;
-$deu_certo = move_uploaded_file($arquivo["tmp_name"], $path);
-
 $stmt = $con->prepare("INSERT INTO pecas (`cod`, `tipo`, `cor`, `marca`, `tamanho`, `comprimento`, `forma_compra`, `forma_venda`,
 `valor_custo`, `valor_pago`, `valor_cheio`, `valor_venda`, `valor_prazo`, `valor_vista`, `parcelas`, `desconto`, `data_compra`, 
 `data_venda`, `descricao`, `med_ombro`, `med_busto`, `med_cintura`, `med_quadril`, `situacao`) 
@@ -81,6 +64,24 @@ $stmt->execute(array(
 ));
 
 if ($stmt->rowCount() > 0) {
+
+    if ($arquivo['error']) {
+        die("Falha ao enviar arquivo");
+    }
+
+
+    $pasta = "arquivos/";
+    $nomeArq = $arquivo['name'];
+    $novoNomeArq = uniqid();
+    $extensao = strtolower(pathinfo($nomeArq, PATHINFO_EXTENSION));
+
+    if ($extensao != "jpg" && $extensao = !"png" && $extensao = !"jpeg") {
+        die("Tipo de arquivo não suportado");
+    }
+
+    $path = "../" . $pasta . $novoNomeArq . "." . $extensao;
+    $deu_certo = move_uploaded_file($arquivo["tmp_name"], $path);
+
     if ($deu_certo) {
         $stmt = $con->prepare("INSERT INTO imagens (caminho, cod_peca) VALUES (:caminho, :cod_peca)");
         $stmt->execute(array(
